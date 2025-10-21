9a4b469653588a7ffa9f6374abc02ebd31fe5511
mgeenens <maxime.geenens@conserto.pro>
2025-10-21 12:03:45 +0200

    Resolve some dependencies

--- common/src/main/java/com/example/demo25/AppConstants.java
+++ common/src/main/java/com/example/demo25/AppConstants.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.example.demo25;
 
 public class AppConstants {
 
--- domain/src/main/java/com/example/demo25/domain/model/Book.java
+++ domain/src/main/java/com/example/demo25/domain/model/Book.java
@@ -2,7 +2,5 @@ package com.example.demo25.domain.model;
 
 import com.example.demo25.domain.enums.Genre;
 
-import java.time.LocalDate;
-
 public record Book(Long id, String title, Author author, Genre genre, String publicationDate) {
 }
